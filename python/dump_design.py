9b87f132c8f0ffa63357096313c92eef0bc5633f
David Shah <davey1576@gmail.com>
2018-06-07 20:35:08 +0200

    Improving dump_design.py
    Signed-off-by: David Shah <davey1576@gmail.com>

--- python/dump_design.py
+++ python/dump_design.py
@@ -1,6 +1,22 @@
 # Run ./nextpnr-ice40 --json ice40/blinky.json --run python/dump_design.py
 for cell in sorted(design.cells, key=lambda x: x.first):
     print("Cell {} : {}".format(cell.first, cell.second.type))
+    print("\tPorts:")
     for port in sorted(cell.second.ports, key=lambda x: x.first):
         dir = (" <-- ", " --> ", " <-> ")[int(port.second.type)]
-        print("    {} {} {}".format(port.first, dir, port.second.net.name))
+        if port.second.net is not None:
+            print("\t\t{} {} {}".format(port.first, dir, port.second.net.name))
+
+    if len(cell.second.attrs) > 0:
+        print("\tAttrs:")
+        for attr in cell.second.attrs:
+            print("\t\t{}: {}".format(attr.first, attr.second))
+
+    if len(cell.second.params) > 0:
+        print("\tParams:")
+        for param in cell.second.params:
+            print("\t\t{}: {}".format(param.first, param.second))
+
+    if not cell.second.bel.nil():
+        print("\tBel: {}".format(chip.getBelName(cell.second.bel)))
+    print()
