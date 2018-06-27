53ce81343aea61e12f5268c362fbb9ccaf792f86
David Shah <davey1576@gmail.com>
2018-06-27 12:23:12 +0200

    ice40: Add timing paths for carry
    Signed-off-by: David Shah <davey1576@gmail.com>

--- ice40/arch.cc
+++ ice40/arch.cc
@@ -494,6 +494,15 @@ bool Arch::getCellDelay(const CellInfo *cell, IdString fromPort, IdString toPort
                 delay = 450;
                 return true;
             }
+        } else if (fromPort == id("CI") && toPort == id("CO")) {
+            delay = 120;
+            return true;
+        } else if (fromPort == id("I1") && toPort == id("CO")) {
+            delay = 260;
+            return true;
+        } else if (fromPort == id("I2") && toPort == id("CO")) {
+            delay = 230;
+            return true;
         }
     }
     return false;
