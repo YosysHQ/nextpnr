1a8b4591f56a2e62a601a32508f3d49ae0c7f112
Miodrag Milanovic <mmicko@gmail.com>
2018-07-03 20:17:20 +0200

    added parameter to call

--- ice40/family.cmake
+++ ice40/family.cmake
@@ -17,7 +17,7 @@ foreach (dev ${devices})
     set(DEV_TXT_DB /usr/local/share/icebox/chipdb-${dev}.txt)
     set(DEV_CC_DB ${CMAKE_CURRENT_SOURCE_DIR}/ice40/chipdbs/chipdb-${dev}.cc)
     add_custom_command(OUTPUT ${DEV_CC_DB}
-            COMMAND python3 ${DB_PY} ${DEV_TXT_DB} > ${DEV_CC_DB}.new
+            COMMAND python3 ${DB_PY} -c ${DEV_TXT_DB} > ${DEV_CC_DB}.new
             COMMAND mv ${DEV_CC_DB}.new ${DEV_CC_DB}
             DEPENDS ${DEV_TXT_DB} ${DB_PY}
             )
