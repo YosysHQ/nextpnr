beb5f9d4df1685a39a02548147e0c7ada40eae48
Miodrag Milanovic <mmicko@gmail.com>
2018-08-04 11:37:41 +0200

    not needed anymore

--- gui/ice40/mainwindow.cc
+++ gui/ice40/mainwindow.cc
@@ -78,18 +78,11 @@ void MainWindow::createMenu()
     menuDesign->addAction(actionSaveAsc);
 }
 
-#if defined(_MSC_VER)
-void load_chipdb();
-#endif
-
 static const ChipInfoPOD *get_chip_info(const RelPtr<ChipInfoPOD> *ptr) { return ptr->get(); }
 
 QStringList getSupportedPackages(ArchArgs::ArchArgsTypes chip)
 {
     QStringList packages;
-#if defined(_MSC_VER)
-    load_chipdb();
-#endif
     const ChipInfoPOD *chip_info;
 #ifdef ICE40_HX1K_ONLY
     if (chip == ArchArgs::HX1K) {
