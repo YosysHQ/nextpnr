1b3a201a54d36085a0675ebaebda523645ad45b3
David Shah <davey1576@gmail.com>
2018-08-19 17:10:55 +0100

    ecp5: Fix delay heuristic
    Signed-off-by: David Shah <davey1576@gmail.com>

--- ecp5/arch.cc
+++ ecp5/arch.cc
@@ -372,7 +372,7 @@ BelId Arch::getBelByLocation(Loc loc) const
 
 delay_t Arch::estimateDelay(WireId src, WireId dst) const
 {
-    return 50 * (abs(src.location.x - dst.location.x) + abs(src.location.y - dst.location.y));
+    return 100 * (abs(src.location.x - dst.location.x) + abs(src.location.y - dst.location.y));
 }
 
 delay_t Arch::predictDelay(const NetInfo *net_info, const PortRef &sink) const
@@ -381,7 +381,7 @@ delay_t Arch::predictDelay(const NetInfo *net_info, const PortRef &sink) const
     auto driver_loc = getBelLocation(driver.cell->bel);
     auto sink_loc = getBelLocation(sink.cell->bel);
 
-    return 50 * (abs(driver_loc.x - sink_loc.x) + abs(driver_loc.y - sink_loc.y));
+    return 100 * (abs(driver_loc.x - sink_loc.x) + abs(driver_loc.y - sink_loc.y));
 }
 
 bool Arch::getBudgetOverride(const NetInfo *net_info, const PortRef &sink, delay_t &budget) const
