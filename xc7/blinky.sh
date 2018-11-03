e137a9c5072f0f02a99bc68a46f6ca854da9783c
Eddie Hung <eddieh@ece.ubc.ca>
2018-11-03 16:12:11 -0700

    [xc7] Add xdl and bitgen to blinky.sh

--- xc7/blinky.sh
+++ xc7/blinky.sh
@@ -2,6 +2,8 @@
 set -ex
 yosys blinky.ys
 ../nextpnr-xc7 --json blinky.json --pcf blinky.pcf --xdl blinky.xdl
+xdl -xdl2ncd blinky.xdl
+bitgen -w blinky.ncd -g UnconstrainedPins:Allow
 #icepack blinky.asc blinky.bin
 #icebox_vlog blinky.asc > blinky_chip.v
 #iverilog -o blinky_tb blinky_chip.v blinky_tb.v
