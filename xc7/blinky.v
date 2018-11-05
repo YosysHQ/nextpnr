f1b454b96ccc6a8799aedd15c1aa67fedf9dff76
Eddie Hung <eddieh@ece.ubc.ca>
2018-11-04 22:13:24 -0800

    [xc7] blinky.v to use led0-3, and just to be safe, set BUFGCTRL control inputs too

--- xc7/blinky.v
+++ xc7/blinky.v
@@ -1,15 +1,19 @@
 module blinky (
     input  clki,
+    output led0,
     output led1,
     output led2,
-    output led3,
-    output led4,
+    output led3
 );
 
     BUFGCTRL clk_gb (
         .I0(clki),
         .CE0(1'b1),
+        .CE1(1'b0),
         .S0(1'b1),
+        .S1(1'b0),
+        .IGNORE0(1'b0),
+        .IGNORE1(1'b0),
         .O(clk)
     );
 
@@ -24,5 +28,5 @@ module blinky (
         outcnt <= counter >> LOG2DELAY;
     end
 
-    assign {led1, led2, led3, led4} = outcnt ^ (outcnt >> 1);
+    assign {led0, led1, led2, led3} = outcnt ^ (outcnt >> 1);
 endmodule
