4574a57efc532621bf96b10583347bae0423dec6
Eddie Hung <eddieh@ece.ubc.ca>
2018-11-30 15:24:32 -0800

    Add attosoc

---
+++ xc7/attosoc_top.v
@@ -0,0 +1,15 @@
+module top (
+	input clk,
+	output [3:0] led
+);
+
+    (* keep *)
+    wire led_unused;
+
+    wire gclk;
+    //clk_wiz_v3_6 pll(.CLK_IN1(clk), .CLK_OUT1(gclk));
+    assign gclk = clk;
+    attosoc soc(.clk(gclk), .led({led_unused, led}));
+
+endmodule
+
