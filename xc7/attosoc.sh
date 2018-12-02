8f0e8888156f055c8713c19fddc15107356344e1
Eddie Hung <eddieh@ece.ubc.ca>
2018-12-02 15:40:20 -0800

    nextpnr now writes to log, netgen to overwrite

--- xc7/attosoc.sh
+++ xc7/attosoc.sh
@@ -3,10 +3,10 @@
 #rm -f picorv32.v
 #wget https://raw.githubusercontent.com/cliffordwolf/picorv32/master/picorv32.v
 yosys attosoc.ys
-../nextpnr-xc7 --json attosoc.json --xdl attosoc.xdl --pcf attosoc.pcf --freq 150
+../nextpnr-xc7 --json attosoc.json --xdl attosoc.xdl --pcf attosoc.pcf --freq 150 |& tee attosoc.log
 xdl -xdl2ncd attosoc.xdl
 bitgen -w attosoc.ncd -g UnconstrainedPins:Allow
 trce attosoc.ncd -v 10
 
-netgen -sim -ofmt vhdl attosoc.ncd attosoc_pnr.vhd
+netgen -sim -ofmt vhdl attosoc.ncd -w attosoc_pnr.vhd
 ghdl -c -fexplicit --no-vital-checks --ieee=synopsys -Pxilinx-ise attosoc_tb.vhd attosoc_pnr.vhd -r testbench
