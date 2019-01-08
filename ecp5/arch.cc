af3ff143be312b0f73289955bd513925f2bb7c4f
David Shah <dave@ds0.me>
2019-01-08 10:52:03 +0000

    ecp5: Improve delay model
    Signed-off-by: David Shah <dave@ds0.me>

--- ecp5/arch.cc
+++ ecp5/arch.cc
@@ -448,16 +448,17 @@ delay_t Arch::estimateDelay(WireId src, WireId dst) const
 
     auto src_loc = est_location(src), dst_loc = est_location(dst);
 
-    return (240 - 20 * args.speed) * (abs(src_loc.first - dst_loc.first) + abs(src_loc.second - dst_loc.second));
+    return (110 - 10 * args.speed) + (200 - 20 * args.speed) *  (abs(src_loc.first - dst_loc.first) + abs(src_loc.second - dst_loc.second));
 }
 
 delay_t Arch::predictDelay(const NetInfo *net_info, const PortRef &sink) const
 {
     const auto &driver = net_info->driver;
+    if (driver.port == id_FCO && sink.port == id_FCI)
+        return 0;
     auto driver_loc = getBelLocation(driver.cell->bel);
     auto sink_loc = getBelLocation(sink.cell->bel);
-
-    return (240 - 20 * args.speed) * (abs(driver_loc.x - sink_loc.x) + abs(driver_loc.y - sink_loc.y));
+    return (110 - 10 * args.speed) + (200 - 20 * args.speed) * (abs(driver_loc.x - sink_loc.x) + abs(driver_loc.y - sink_loc.y));
 }
 
 bool Arch::getBudgetOverride(const NetInfo *net_info, const PortRef &sink, delay_t &budget) const { return false; }
