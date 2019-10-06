d1dc2c3a5f4fc1d737eeac7d0fb9c87bf89c392b
Miodrag Milanovic <mmicko@gmail.com>
2019-10-06 18:09:23 +0200

    Add more zoom

--- gui/fpgaviewwidget.h
+++ gui/fpgaviewwidget.h
@@ -126,7 +126,7 @@ class FPGAViewWidget : public QOpenGLWidget, protected QOpenGLFunctions
     void clickedPip(PipId pip, bool add);
 
   private:
-    const float zoomNear_ = 0.1f; // do not zoom closer than this
+    const float zoomNear_ = 0.05f; // do not zoom closer than this
     float zoomFar_ = 10.0f;       // do not zoom further than this
     const float zoomLvl1_ = 1.0f;
     const float zoomLvl2_ = 5.0f;
