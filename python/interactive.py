2898d8182da79da8ebe238571760006436dfc19d
David Shah <dave@ds0.me>
2019-11-11 09:44:11 +0000

    Fix typo
    Signed-off-by: David Shah <dave@ds0.me>

--- python/interactive.py
+++ python/interactive.py
@@ -1,5 +1,5 @@
 # Pass this file to one of the Python script arguments (e.g. --pre-place interactive.py)
-# to drop to an command-line interactive Python session in the middle of place and route 
+# to drop to a command-line interactive Python session in the middle of place and route 
 
 import code
 print("Press Ctrl+D to finish interactive session")
