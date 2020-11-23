75fcc7fcff620c3fa941e6d1012938e4db5e6838
William D. Jones <thor0505@comcast.net>
2020-11-23 16:56:41 -0500

    machxo2: Add constant packing implementation, fix bugs in create_machxo2_cell.

--- machxo2/pack.cc
+++ machxo2/pack.cc
@@ -27,6 +27,75 @@
 
 NEXTPNR_NAMESPACE_BEGIN
 
+// Merge a net into a constant net
+static void set_net_constant(const Context *ctx, NetInfo *orig, NetInfo *constnet, bool constval)
+{
+    (void) constval;
+
+    orig->driver.cell = nullptr;
+    for (auto user : orig->users) {
+        if (user.cell != nullptr) {
+            CellInfo *uc = user.cell;
+            if (ctx->verbose)
+                log_info("%s user %s\n", orig->name.c_str(ctx), uc->name.c_str(ctx));
+
+            uc->ports[user.port].net = constnet;
+            constnet->users.push_back(user);
+        }
+    }
+    orig->users.clear();
+}
+
+
+// Pack constants (based on simple implementation in generic).
+// VCC/GND cells provided by nextpnr automatically.
+static void pack_constants(Context *ctx)
+{
+    log_info("Packing constants..\n");
+
+    std::unique_ptr<CellInfo> const_cell = create_machxo2_cell(ctx, id_FACADE_SLICE, "$PACKER_CONST");
+    const_cell->params[id_LUT0_INITVAL] = Property(0, 16);
+    const_cell->params[id_LUT1_INITVAL] = Property(0xFFFF, 16);
+
+    std::unique_ptr<NetInfo> gnd_net = std::unique_ptr<NetInfo>(new NetInfo);
+    gnd_net->name = ctx->id("$PACKER_GND_NET");
+    gnd_net->driver.cell = const_cell.get();
+    gnd_net->driver.port = id_F0;
+    const_cell->ports.at(id_F0).net = gnd_net.get();
+
+    std::unique_ptr<NetInfo> vcc_net = std::unique_ptr<NetInfo>(new NetInfo);
+    vcc_net->name = ctx->id("$PACKER_VCC_NET");
+    vcc_net->driver.cell = const_cell.get();
+    vcc_net->driver.port = id_F1;
+    const_cell->ports.at(id_F1).net = vcc_net.get();
+
+    std::vector<IdString> dead_nets;
+
+    for (auto net : sorted(ctx->nets)) {
+        NetInfo *ni = net.second;
+        if (ni->driver.cell != nullptr && ni->driver.cell->type == ctx->id("GND")) {
+            IdString drv_cell = ni->driver.cell->name;
+            set_net_constant(ctx, ni, gnd_net.get(), false);
+            dead_nets.push_back(net.first);
+            ctx->cells.erase(drv_cell);
+        } else if (ni->driver.cell != nullptr && ni->driver.cell->type == ctx->id("VCC")) {
+            IdString drv_cell = ni->driver.cell->name;
+            set_net_constant(ctx, ni, vcc_net.get(), true);
+            dead_nets.push_back(net.first);
+            ctx->cells.erase(drv_cell);
+        }
+    }
+
+    ctx->cells[const_cell->name] = std::move(const_cell);
+    ctx->nets[gnd_net->name] = std::move(gnd_net);
+    ctx->nets[vcc_net->name] = std::move(vcc_net);
+
+    for (auto dn : dead_nets) {
+        ctx->nets.erase(dn);
+    }
+}
+
+
 // Main pack function
 bool Arch::pack()
 {
@@ -34,6 +103,7 @@ bool Arch::pack()
     log_info("Packing implementation goes here..");
     try {
         log_break();
+        pack_constants(ctx);
         ctx->settings[ctx->id("pack")] = 1;
         ctx->assignArchInfo();
         log_info("Checksum: 0x%08x\n", ctx->checksum());
