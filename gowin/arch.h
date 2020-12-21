f54c8189f1ab2cf124b917cd08fd2a5b94d7dc2b
Pepijn de Vos <pepijndevos@gmail.com>
2020-12-21 12:16:22 +0100

    increase ripup and epsilon a bit

--- gowin/arch.h
+++ gowin/arch.h
@@ -384,8 +384,8 @@ struct Arch : BaseCtx
 
     delay_t estimateDelay(WireId src, WireId dst) const;
     delay_t predictDelay(const NetInfo *net_info, const PortRef &sink) const;
-    delay_t getDelayEpsilon() const { return 0.001; }
-    delay_t getRipupDelayPenalty() const { return 0.015; }
+    delay_t getDelayEpsilon() const { return 0.01; }
+    delay_t getRipupDelayPenalty() const { return 0.4; }
     float getDelayNS(delay_t v) const { return v; }
 
     DelayInfo getDelayFromNS(float ns) const
