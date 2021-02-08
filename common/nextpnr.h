2932dc3985181afbf88a87e58ee27dfcca433625
gatecat <gatecat@ds0.me>
2021-02-08 17:03:22 +0000

    Make BaseCtx destructor virtual
    Signed-off-by: gatecat <gatecat@ds0.me>

--- common/nextpnr.h
+++ common/nextpnr.h
@@ -858,7 +858,7 @@ struct BaseCtx
         constraintObjects.push_back(wildcard);
     }
 
-    ~BaseCtx()
+    virtual ~BaseCtx()
     {
         delete idstring_str_to_idx;
         delete idstring_idx_to_str;
