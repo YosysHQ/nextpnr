8a860857ea8a1a0fffd68762bcf4cd8432e59408
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-02-11 15:36:16 -0800

    Remove capnp and libz for XDC parser PR.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- fpga_interchange/family.cmake
+++ fpga_interchange/family.cmake
@@ -1,4 +1,3 @@
-
 find_package(TCL)
 if(NOT ${TCL_FOUND})
     message(FATAL_ERROR "Tcl is required for FPGA interchange Arch.")
@@ -7,7 +6,4 @@ endif()
 foreach (target ${family_targets})
     target_link_libraries(${target} LINK_PUBLIC ${TCL_LIBRARY})
     include_directories (${TCL_INCLUDE_PATH})
-    target_link_libraries(${target} LINK_PUBLIC capnp)
-    target_link_libraries(${target} LINK_PUBLIC kj)
-    target_link_libraries(${target} LINK_PUBLIC z)
 endforeach()
