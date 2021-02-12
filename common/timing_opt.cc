d3688982acacc36b423eaa230df4ea99352afe6f
gatecat <gatecat@ds0.me>
2021-02-12 11:16:59 +0000

    Fix bad rebase
    Signed-off-by: gatecat <gatecat@ds0.me>

--- common/timing_opt.cc
+++ common/timing_opt.cc
@@ -59,8 +59,7 @@ template <> struct hash<std::pair<int, NEXTPNR_NAMESPACE_PREFIX BelId>>
         return seed;
     }
 };
-
-#if !(defined(ARCH_GENERIC) || defined(ARCH_GOWIN))
+#if !defined(ARCH_GOWIN)
 template <> struct hash<std::pair<NEXTPNR_NAMESPACE_PREFIX IdString, NEXTPNR_NAMESPACE_PREFIX BelId>>
 {
     std::size_t
