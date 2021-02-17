9e0ca7282743afd6a17fe347c1ad3a5d1cd4070d
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-02-17 09:31:55 -0800

    Keep all build artifacts under create_bba/build.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- fpga_interchange/examples/common.mk
+++ fpga_interchange/examples/common.mk
@@ -1,6 +1,6 @@
 NEXTPNR_PATH := $(realpath ../../..)
 NEXTPNR_BIN := $(NEXTPNR_PATH)/build/nextpnr-fpga_interchange
-BBA_PATH := $(realpath ..)/create_bba/build/test.bin
+BBA_PATH := $(realpath ..)/create_bba/build/xc7a35tcpg236-1.bin
 
 RAPIDWRIGHT_PATH := $(realpath ..)/create_bba/build/RapidWright
 INTERCHANGE_PATH := $(realpath ..)/create_bba/build/fpga-interchange-schema/interchange
