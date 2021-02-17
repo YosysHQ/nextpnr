cc687b3b726d538be4a8dfa6cb8e1b4b96a837e2
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-02-16 17:22:24 -0800

    Change makefiles to build a FPGA interchange BBA.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- fpga_interchange/examples/template.mk
+++ fpga_interchange/examples/template.mk
@@ -1,12 +1,4 @@
-NEXTPNR_PATH := $(shell echo ~/cat_x/nextpnr)
-NEXTPNR_BIN := $(NEXTPNR_PATH)/build/nextpnr-fpga_interchange
-BBA_PATH := $(NEXTPNR_PATH)/build/test.bin
-
-RAPIDWRIGHT_PATH := $(shell echo ~/cat_x/RapidWright)
-
-INTERCHANGE_PATH := $(NEXTPNR_PATH)/3rdparty/fpga-interchange-schema/interchange
-
-DEVICE := $(shell echo ~/cat_x/python-fpga-interchange/xc7a35tcpg236-1_constraints_luts.device)
+include ../common.mk
 
 .DELETE_ON_ERROR:
 .PHONY: all debug clean
@@ -39,7 +31,7 @@ build/$(DESIGN).phys: build/$(DESIGN).netlist
 		--xdc $(DESIGN).xdc \
 		--netlist build/$(DESIGN).netlist \
 		--phys build/$(DESIGN).phys \
-		--package $(PACKAGE)
+		--package $(PACKAGE) \
 
 build/$(DESIGN)_phys.yaml: build/$(DESIGN).phys
 	/usr/bin/time -v python3 -mfpga_interchange.convert \
@@ -64,5 +56,5 @@ build/$(DESIGN).dcp: build/$(DESIGN).netlist build/$(DESIGN).phys $(DESIGN).xdc
 		com.xilinx.rapidwright.interchange.PhysicalNetlistToDcp \
 		build/$(DESIGN).netlist build/$(DESIGN).phys $(DESIGN).xdc build/$(DESIGN).dcp
 
-clean::
+clean:
 	rm -rf build
