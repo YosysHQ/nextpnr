0abe425675ae0cc550194171f1f15ec4058a05d6
gatecat <gatecat@ds0.me>
2021-04-25 11:53:21 +0100

    nexus: Enable placeAllAtOnce
    Signed-off-by: gatecat <gatecat@ds0.me>

--- nexus/arch.cc
+++ nexus/arch.cc
@@ -632,10 +632,7 @@ bool Arch::place()
         cfg.ioBufTypes.insert(id_SEIO33_CORE);
         cfg.ioBufTypes.insert(id_SEIO18_CORE);
         cfg.ioBufTypes.insert(id_OSC_CORE);
-        cfg.cellGroups.emplace_back();
-        cfg.cellGroups.back().insert({id_OXIDE_COMB});
-        cfg.cellGroups.back().insert({id_OXIDE_FF});
-
+        cfg.placeAllAtOnce = true;
         cfg.beta = 0.5;
         cfg.criticalityExponent = 7;
         if (!placer_heap(getCtx(), cfg))
