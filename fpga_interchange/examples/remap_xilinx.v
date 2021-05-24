2a3d0a228c4e1352810da213a313b8cd1c7f5f37
Jan Kowalewski <jkowalewski@antmicro.com>
2021-05-24 14:09:34 +0200

    interchange: xilinx: techmap carry fixups
    Signed-off-by: Jan Kowalewski <jkowalewski@antmicro.com>

--- fpga_interchange/examples/remap_xilinx.v
+++ fpga_interchange/examples/remap_xilinx.v
@@ -28,33 +28,32 @@ module CARRY4(
   localparam [0:0] IS_CI_ONE = (
       _TECHMAP_CONSTMSK_CI_ == 1 && _TECHMAP_CONSTVAL_CI_ == 0 &&
       _TECHMAP_CONSTMSK_CYINIT_ == 1 && _TECHMAP_CONSTVAL_CYINIT_ == 1);
+  localparam [0:0] IS_CYINIT_FABRIC = _TECHMAP_CONSTMSK_CYINIT_ == 0;
 
-  /* Workaround for LUT-thrus, add LUTs explicitly to reduce site_routing
-   * time consumption
-   */
-  wire S0, S1, S2, S3;
-  LUT1 #(.INIT(2'b10)) LUT_S0 (.I0(S[0]), .O(S0));
-  LUT1 #(.INIT(2'b10)) LUT_S1 (.I0(S[1]), .O(S1));
-  LUT1 #(.INIT(2'b10)) LUT_S2 (.I0(S[2]), .O(S2));
-  LUT1 #(.INIT(2'b10)) LUT_S3 (.I0(S[3]), .O(S3));
-
-
-  if(IS_CI_ZERO || IS_CI_ONE) begin
+  if(IS_CYINIT_FABRIC) begin
     CARRY4 _TECHMAP_REPLACE_ (
-        .CYINIT(CYINIT),
         .CO(CO),
+        .CYINIT(CYINIT),
         .O(O),
         .DI(DI),
-        .S({S0, S1, S2, S3}),
+        .S(S),
+    );
+  end else if(IS_CI_ZERO || IS_CI_ONE) begin
+    CARRY4 _TECHMAP_REPLACE_ (
+        .CO(CO),
+        .CYINIT(CYINIT),
+        .O(O),
+        .DI(DI),
+        .S(S),
     );
   end else begin
     CARRY4 _TECHMAP_REPLACE_ (
-        .CI(CI),
-        .CYINIT(CYINIT),
         .CO(CO),
+        .CYINIT(CYINIT),
         .O(O),
         .DI(DI),
-        .S({S0, S1, S2, S3}),
+        .S(S),
+        .CI(CI)
     );
   end
 endmodule
