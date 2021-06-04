47f24a702464e5afbf04e72ea4acd79615b0b0ef
gatecat <gatecat@ds0.me>
2021-06-04 19:21:09 +0100

    mistral: Build libmistral as a cmake subdir
    Signed-off-by: gatecat <gatecat@ds0.me>

--- mistral/family.cmake
+++ mistral/family.cmake
@@ -1,13 +1,12 @@
 set(MISTRAL_ROOT "" CACHE STRING "Mistral install path")
+set(MISTRAL_DONT_INSTALL ON)
 
-aux_source_directory(${MISTRAL_ROOT}/lib MISTRAL_LIB_FILES)
-add_library(mistral STATIC ${MISTRAL_LIB_FILES})
-target_compile_options(mistral PRIVATE -Wno-maybe-uninitialized -Wno-uninitialized -Wno-unknown-warning-option)
+add_subdirectory(${MISTRAL_ROOT}/libmistral ${CMAKE_CURRENT_BINARY_DIR}/libmistral)
 
 find_package(LibLZMA REQUIRED)
 
 foreach(family_target ${family_targets})
-    target_include_directories(${family_target} PRIVATE ${MISTRAL_ROOT}/lib ${LIBLZMA_INCLUDE_DIRS})
+    target_include_directories(${family_target} PRIVATE ${MISTRAL_ROOT}/libmistral ${LIBLZMA_INCLUDE_DIRS})
     target_link_libraries(${family_target} PRIVATE mistral ${LIBLZMA_LIBRARIES})
 	# Currently required to avoid issues with mistral (LTO means the warnings can end up in nextpnr)
 	target_link_options(${family_target} PRIVATE -Wno-maybe-uninitialized -Wno-uninitialized -Wno-unknown-warning-option)
