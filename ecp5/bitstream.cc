a66cd0200bdcb87f17f97fb7a65bfd99c42a6de2
gatecat <gatecat@ds0.me>
2021-08-14 20:22:54 +0100

    clangformat
    Signed-off-by: gatecat <gatecat@ds0.me>

--- ecp5/bitstream.cc
+++ ecp5/bitstream.cc
@@ -985,9 +985,10 @@ void write_bitstream(Context *ctx, std::string base_config_file, std::string tex
                         other = "PIOD";
                     else
                         log_error("cannot set OPENDRAIN on differential IO at location %s\n", pio.c_str());
-                    cc.tiles[pio_tile].add_enum(other + ".OPENDRAIN", str_or_default(ci->attrs, ctx->id("OPENDRAIN"), "OFF"));
+                    cc.tiles[pio_tile].add_enum(other + ".OPENDRAIN",
+                                                str_or_default(ci->attrs, ctx->id("OPENDRAIN"), "OFF"));
                 }
-            } 
+            }
 
             std::string datamux_oddr = str_or_default(ci->params, ctx->id("DATAMUX_ODDR"), "PADDO");
             if (datamux_oddr != "PADDO")
