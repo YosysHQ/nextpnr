f7be385230cbd6b6a030c8e159ac2cf175298c16
gatecat <gatecat@ds0.me>
2021-08-15 15:13:31 +0100

    mistral: Include mistral generated files in include dirs
    Signed-off-by: gatecat <gatecat@ds0.me>

--- mistral/family.cmake
+++ mistral/family.cmake
@@ -6,7 +6,7 @@ add_subdirectory(${MISTRAL_ROOT}/libmistral ${CMAKE_CURRENT_BINARY_DIR}/libmistr
 find_package(LibLZMA REQUIRED)
 
 foreach(family_target ${family_targets})
-    target_include_directories(${family_target} PRIVATE ${MISTRAL_ROOT}/libmistral ${LIBLZMA_INCLUDE_DIRS})
+    target_include_directories(${family_target} PRIVATE ${MISTRAL_ROOT}/libmistral ${CMAKE_CURRENT_BINARY_DIR}/libmistral ${LIBLZMA_INCLUDE_DIRS})
     target_link_libraries(${family_target} PRIVATE mistral ${LIBLZMA_LIBRARIES})
 	# Currently required to avoid issues with mistral (LTO means the warnings can end up in nextpnr)
 	target_link_options(${family_target} PRIVATE -Wno-maybe-uninitialized -Wno-uninitialized -Wno-unknown-warning-option)
