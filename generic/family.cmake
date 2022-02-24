fbb02e286047108dbfbd6bd71cddc777a6752ecf
Lofty <dan.ravensloft@gmail.com>
2022-02-24 20:29:43 +0000

    okami: new Viaduct arch

--- generic/family.cmake
+++ generic/family.cmake
@@ -1,4 +1,4 @@
-set(VIADUCT_UARCHES "example")
+set(VIADUCT_UARCHES "example" "okami")
 foreach(uarch ${VIADUCT_UARCHES})
     aux_source_directory(${family}/viaduct/${uarch} UARCH_FILES)
     foreach(target ${family_targets})
---
+++ generic/viaduct/okami/synth_okami.tcl
@@ -0,0 +1,24 @@
+# Usage
+# tcl synth_okami.tcl {out.json}
+
+yosys read_verilog -lib [file dirname [file normalize $argv0]]/okami_prims.v
+yosys hierarchy -check -top top
+yosys proc
+yosys flatten
+yosys tribuf -logic
+yosys deminout
+yosys synth -run coarse
+yosys memory_map
+yosys opt -full
+yosys iopadmap -bits -inpad INBUF O:PAD -outpad OUTBUF I:PAD
+yosys techmap -map +/techmap.v
+yosys opt -fast
+yosys dfflegalize -cell \$_DFF_P_ 0
+yosys abc -lut 4 -dress
+yosys clean
+yosys techmap -map [file dirname [file normalize $argv0]]/okami_map.v
+yosys clean
+yosys hierarchy -check
+yosys stat
+
+if {$argc > 0} { yosys write_json [lindex $argv 0] }
