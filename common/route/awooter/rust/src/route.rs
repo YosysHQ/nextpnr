76458e88be5e82439e3b8758a1b82bde81e7525c
Lofty <dan.ravensloft@gmail.com>
2022-11-27 16:26:17 +0000

    awooter: further clean up

--- common/route/awooter/rust/src/route.rs
+++ common/route/awooter/rust/src/route.rs
@@ -14,5 +14,27 @@ impl Arc {
             source_wire, source_loc, sink_wire, sink_loc, net
         }
     }
+
+    pub fn split(&self, ctx: &npnr::Context, pip: npnr::PipId) -> (Self, Self) {
+        let pip_src = ctx.pip_src_wire(pip);
+        let pip_dst = ctx.pip_dst_wire(pip);
+        (Self {
+            source_wire: self.source_wire,
+            source_loc: self.source_loc,
+            sink_wire: pip_src,
+            sink_loc: ctx.pip_location(pip),
+            net: self.net
+        },
+        Self {
+            source_wire: pip_dst,
+            source_loc: ctx.pip_location(pip),
+            sink_wire: self.sink_wire,
+            sink_loc: self.sink_loc,
+            net: self.net
+        })
+    }
 }
 
+pub struct Router {
+    arcs: Vec<Arc>
+}
