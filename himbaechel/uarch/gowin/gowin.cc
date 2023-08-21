9bfb0cfe626db5dfae2b501f5e884b614840cdec
YRabbit <rabbit@yrabbit.cyou>
2023-08-21 19:52:38 +1000

    gowin: Himbaechel. Handling of disabled units
    Using  extra cell functions to mark disabled units using the PLL example.
    
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- himbaechel/uarch/gowin/gowin.cc
+++ himbaechel/uarch/gowin/gowin.cc
@@ -113,14 +113,10 @@ void GowinImpl::init(Context *ctx)
         }
     }
 
-    if (ctx->debug) {
-        log_info("packages:%ld\n", ctx->chip_info->packages.ssize());
-    }
+    // log_info("packages:%ld\n", ctx->chip_info->packages.ssize());
     for (int i = 0; i < ctx->chip_info->packages.ssize(); ++i) {
         if (IdString(ctx->chip_info->packages[i].name) == package_idx) {
-            if (ctx->debug) {
-                log_info("i:%d %s\n", i, package_idx.c_str(ctx));
-            }
+            // log_info("i:%d %s\n", i, package_idx.c_str(ctx));
             ctx->package_info = &ctx->chip_info->packages[i];
             break;
         }
