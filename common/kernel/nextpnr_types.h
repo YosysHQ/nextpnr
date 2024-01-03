bb74b5b27c2ed975b0f6f73c2bb39c1cbaa00443
dragonmux <git@dragonmux.network>
2024-01-03 15:02:59 +0000

    common/kernel: Fixed unused parameters and spurious `;` warnings in some of the headers

--- common/kernel/nextpnr_types.h
+++ common/kernel/nextpnr_types.h
@@ -81,10 +81,10 @@ struct PortRef
 struct DelayPair
 {
     DelayPair(){};
-    explicit DelayPair(delay_t delay) : min_delay(delay), max_delay(delay){};
-    DelayPair(delay_t min_delay, delay_t max_delay) : min_delay(min_delay), max_delay(max_delay){};
-    delay_t minDelay() const { return min_delay; };
-    delay_t maxDelay() const { return max_delay; };
+    explicit DelayPair(delay_t delay) : min_delay(delay), max_delay(delay) { }
+    DelayPair(delay_t min_delay, delay_t max_delay) : min_delay(min_delay), max_delay(max_delay) { }
+    delay_t minDelay() const { return min_delay; }
+    delay_t maxDelay() const { return max_delay; }
     delay_t min_delay, max_delay;
     DelayPair operator+(const DelayPair &other) const
     {
@@ -100,21 +100,21 @@ struct DelayPair
 struct DelayQuad
 {
     DelayPair rise, fall;
-    DelayQuad(){};
-    explicit DelayQuad(delay_t delay) : rise(delay), fall(delay){};
-    DelayQuad(delay_t min_delay, delay_t max_delay) : rise(min_delay, max_delay), fall(min_delay, max_delay){};
-    DelayQuad(DelayPair rise, DelayPair fall) : rise(rise), fall(fall){};
+    DelayQuad() { }
+    explicit DelayQuad(delay_t delay) : rise(delay), fall(delay) { }
+    DelayQuad(delay_t min_delay, delay_t max_delay) : rise(min_delay, max_delay), fall(min_delay, max_delay) { }
+    DelayQuad(DelayPair rise, DelayPair fall) : rise(rise), fall(fall) { }
     DelayQuad(delay_t min_rise, delay_t max_rise, delay_t min_fall, delay_t max_fall)
-            : rise(min_rise, max_rise), fall(min_fall, max_fall){};
+            : rise(min_rise, max_rise), fall(min_fall, max_fall) { }
 
-    delay_t minRiseDelay() const { return rise.minDelay(); };
-    delay_t maxRiseDelay() const { return rise.maxDelay(); };
-    delay_t minFallDelay() const { return fall.minDelay(); };
-    delay_t maxFallDelay() const { return fall.maxDelay(); };
-    delay_t minDelay() const { return std::min<delay_t>(rise.minDelay(), fall.minDelay()); };
-    delay_t maxDelay() const { return std::max<delay_t>(rise.maxDelay(), fall.maxDelay()); };
+    delay_t minRiseDelay() const { return rise.minDelay(); }
+    delay_t maxRiseDelay() const { return rise.maxDelay(); }
+    delay_t minFallDelay() const { return fall.minDelay(); }
+    delay_t maxFallDelay() const { return fall.maxDelay(); }
+    delay_t minDelay() const { return std::min<delay_t>(rise.minDelay(), fall.minDelay()); }
+    delay_t maxDelay() const { return std::max<delay_t>(rise.maxDelay(), fall.maxDelay()); }
 
-    DelayPair delayPair() const { return DelayPair(minDelay(), maxDelay()); };
+    DelayPair delayPair() const { return DelayPair(minDelay(), maxDelay()); }
 
     DelayQuad operator+(const DelayQuad &other) const { return {rise + other.rise, fall + other.fall}; }
     DelayQuad operator-(const DelayQuad &other) const { return {rise - other.rise, fall - other.fall}; }
@@ -124,7 +124,7 @@ struct ClockConstraint;
 
 struct NetInfo : ArchNetInfo
 {
-    explicit NetInfo(IdString name) : name(name){};
+    explicit NetInfo(IdString name) : name(name) { }
     IdString name, hierpath;
     int32_t udata = 0;
 
@@ -203,14 +203,14 @@ struct PseudoCell
 
 struct RegionPlug : PseudoCell
 {
-    RegionPlug(Loc loc) : loc(loc){}; // 'loc' is a notional location for the placer only
+    RegionPlug(Loc loc) : loc(loc) { } // 'loc' is a notional location for the placer only
     Loc getLocation() const override { return loc; }
     WireId getPortWire(IdString port) const override { return port_wires.at(port); }
 
     // TODO: partial reconfiguration region timing
-    bool getDelay(IdString fromPort, IdString toPort, DelayQuad &delay) const override { return false; }
-    TimingPortClass getPortTimingClass(IdString port, int &clockInfoCount) const override { return TMG_IGNORE; }
-    TimingClockingInfo getPortClockingInfo(IdString port, int index) const override { return TimingClockingInfo{}; }
+    bool getDelay(IdString /*fromPort*/, IdString /*toPort*/, DelayQuad &/*delay*/) const override { return false; }
+    TimingPortClass getPortTimingClass(IdString /*port*/, int &/*clockInfoCount*/) const override { return TMG_IGNORE; }
+    TimingClockingInfo getPortClockingInfo(IdString /*port*/, int /*index*/) const override { return TimingClockingInfo{}; }
 
     dict<IdString, WireId> port_wires;
     Loc loc;
@@ -218,7 +218,7 @@ struct RegionPlug : PseudoCell
 
 struct CellInfo : ArchCellInfo
 {
-    CellInfo(Context *ctx, IdString name, IdString type) : ctx(ctx), name(name), type(type){};
+    CellInfo(Context *ctx, IdString name, IdString type) : ctx(ctx), name(name), type(type) { }
     Context *ctx = nullptr;
 
     IdString name, type, hierpath;
