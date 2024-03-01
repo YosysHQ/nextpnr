8b486df0595521e7ebf1fbe807e078f8a536c70d
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-03-01 11:18:13 +0100

    Add devShell to flake.nix. Build every available stable architecture. Remove shell.nix. Add nix github action

--- flake.nix
+++ flake.nix
@@ -23,47 +23,57 @@
         };
         enableGui = false;
       in
-      {
-        packages = {
-          nextpnr = pkgs.stdenv.mkDerivation {
-            pname = "nextpnr";
-            version = "0.6";
-            src = ./.;
+        {
+          packages = {
+            nextpnr = pkgs.stdenv.mkDerivation {
+              pname = "nextpnr";
+              version = "0.6";
+              src = ./.;
 
-            nativeBuildInputs = [ pkgs.cmake ]
-              ++ (pkgs.lib.optional enableGui pkgs.qt6.wrapQtAppsHook);
-            buildInputs = [
-              (pkgs.boost.override { python = pkgs.python3; enablePython = true; })
-              pkgs.python3
-              pkgs.eigen
-              myPythonPackages.apycula
-              pkgs.icestorm
-              pkgs.trellis
-            ] ++ (pkgs.lib.optional enableGui pkgs.qt6.qtbase)
+              nativeBuildInputs = [ pkgs.cmake ]
+                                  ++ (pkgs.lib.optional enableGui pkgs.qt6.wrapQtAppsHook);
+              buildInputs = [
+                (pkgs.boost.override { python = pkgs.python3; enablePython = true; })
+                pkgs.python3
+                pkgs.eigen
+                myPythonPackages.apycula
+                pkgs.icestorm
+                pkgs.trellis
+                pkgs.tcl
+                pkgs.zlib
+                pkgs.capnproto
+                pkgs.lzma
+                pkgs.tk
+                pkgs.wget
+              ] ++ (pkgs.lib.optional enableGui pkgs.qt6.qtbase)
               ++ (pkgs.lib.optional pkgs.stdenv.cc.isClang pkgs.llvmPackages.openmp);
 
-            cmakeFlags = [
-              "-DARCH=generic;ice40;ecp5;gowin"
-              "-DBUILD_TESTS=ON"
-              "-DICESTORM_INSTALL_PREFIX=${pkgs.icestorm}"
-              "-DTRELLIS_INSTALL_PREFIX=${pkgs.trellis}"
-              "-DTRELLIS_LIBDIR=${pkgs.trellis}/lib/trellis"
-              "-DGOWIN_BBA_EXECUTABLE=${myPythonPackages.apycula}/bin/gowin_bba"
-              "-DUSE_OPENMP=ON"
-              # warning: high RAM usage
-              "-DSERIALIZE_CHIPDBS=OFF"
-            ] ++ (pkgs.lib.optional enableGui "-DBUILD_GUI=ON");
+              cmakeFlags = [
+                "-DARCH=all"
+                "-DBUILD_TESTS=ON"
+                "-DICESTORM_INSTALL_PREFIX=${pkgs.icestorm}"
+                "-DTRELLIS_INSTALL_PREFIX=${pkgs.trellis}"
+                "-DTRELLIS_LIBDIR=${pkgs.trellis}/lib/trellis"
+                "-DGOWIN_BBA_EXECUTABLE=${myPythonPackages.apycula}/bin/gowin_bba"
+                "-DUSE_OPENMP=ON"
+                # warning: high RAM usage
+                "-DSERIALIZE_CHIPDBS=OFF"
+              ] ++ (pkgs.lib.optional enableGui "-DBUILD_GUI=ON");
 
-            meta = with pkgs.lib; {
-              description = "Place and route tool for FPGAs";
-              homepage = "https://github.com/yosyshq/nextpnr";
-              license = licenses.isc;
-              platforms = platforms.all;
-              maintainers = with maintainers; [ thoughtpolice emily ];
+              meta = with pkgs.lib; {
+                description = "Place and route tool for FPGAs";
+                homepage = "https://github.com/yosyshq/nextpnr";
+                license = licenses.isc;
+                platforms = platforms.all;
+                maintainers = with maintainers; [ ];
+              };
             };
           };
-        };
 
-        packages.default = self.packages.${system}.nextpnr;
-      });
+          packages.default = self.packages.${system}.nextpnr;
+
+          devShell = pkgs.mkShell {
+            buildInputs = [ self.packages.${system}.default ];
+          };
+        });
 }
