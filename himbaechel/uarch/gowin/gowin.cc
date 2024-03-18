06e3fa77a83b39213c083e3a2cf644716b6b1c06
YRabbit <rabbit@yrabbit.cyou>
2024-03-19 08:06:17 +1000

    gowin: BUGFIX fix typo
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- himbaechel/uarch/gowin/gowin.cc
+++ himbaechel/uarch/gowin/gowin.cc
@@ -86,7 +86,7 @@ void GowinImpl::init_database(Arch *arch)
     } else {
         bool GW2 = args.device.rfind("GW2A", 0) == 0;
         if (GW2) {
-            log_error("For the GW2A series you need to specify -vopt family=GW2A-18 or -vopt family=GW2A-18C\n");
+            log_error("For the GW2A series you need to specify --vopt family=GW2A-18 or --vopt family=GW2A-18C\n");
         } else {
             std::regex devicere = std::regex("GW1N([SZ]?)[A-Z]*-(LV|UV|UX)([0-9])(C?).*");
             std::smatch match;
@@ -95,7 +95,7 @@ void GowinImpl::init_database(Arch *arch)
             }
             family = stringf("GW1N%s-%s", match[1].str().c_str(), match[3].str().c_str());
             if (family.rfind("GW1N-9", 0) == 0) {
-                log_error("For the GW1N-9 series you need to specify -vopt family=GW1N-9 or -vopt family=GW1N-9C\n");
+                log_error("For the GW1N-9 series you need to specify --vopt family=GW1N-9 or --vopt family=GW1N-9C\n");
             }
         }
     }
