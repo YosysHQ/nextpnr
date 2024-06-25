dd6ab3f0d8fbe90593053ddccd727da8990e65d7
YRabbit <rabbit@yrabbit.cyou>
2024-06-25 18:27:00 +1000

    Gowin. Fix pipeline mode in BSRAM.
    It seems that the internal registers on the BSRAM output pins in
    READ_MODE=1'b1 (pipeline) mode do not function properly because in the
    images generated by Gowin IDE an external register is added to each pin,
    and the BSRAM itself switches to READ_MODE=1'b0 (bypass) mode .
    
    This is observed on Tangnano9k and Tangnano20k boards.
    
    Here we repeat this fix.
    
    Signed-off-by: YRabbit <rabbit@yrabbit.cyou>

--- himbaechel/uarch/gowin/gowin.h
+++ himbaechel/uarch/gowin/gowin.h
@@ -97,6 +97,7 @@ NPNR_PACKED_STRUCT(struct Extra_chip_data_POD {
     // chip flags
     static constexpr int32_t HAS_SP32 = 1;
     static constexpr int32_t NEED_SP_FIX = 2;
+    static constexpr int32_t NEED_BSRAM_OUTREG_FIX = 4;
 });
 
 } // namespace
