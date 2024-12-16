7febd7afd54976baf62779fb7e6899f518ab3b88
Miodrag Milanovic <mmicko@gmail.com>
2024-12-16 16:58:46 +0100

    Add CCF parsing

--- himbaechel/uarch/gatemate/gatemate.h
+++ himbaechel/uarch/gatemate/gatemate.h
@@ -45,6 +45,8 @@ struct GateMateImpl : HimbaechelAPI
 
     void drawBel(std::vector<GraphicElement> &g, GraphicElement::style_t style, IdString bel_type, Loc loc) override;
 
+    bool parse_ccf(const std::string &filename);
+
   private:
     HimbaechelHelpers h;
 };
