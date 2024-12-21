12878929e0563562d57a9fe172f093fe66bb9eae
Miodrag Milanovic <mmicko@gmail.com>
2024-12-21 12:15:03 +0100

    Cleanup

--- himbaechel/uarch/gatemate/bitstream.cc
+++ himbaechel/uarch/gatemate/bitstream.cc
@@ -133,7 +133,8 @@ struct BitstreamBackend
                 }
             } break;
             default:
-                break;
+                log_error("Unhandled cell %s of type %s\n", cell.second.get()->name.c_str(ctx),
+                          cell.second->type.c_str(ctx));
             }
         }
 
--- himbaechel/uarch/gatemate/gatemate.cc
+++ himbaechel/uarch/gatemate/gatemate.cc
@@ -105,7 +105,8 @@ void GateMateImpl::setupArchContext()
 // Bel bucket functions
 IdString GateMateImpl::getBelBucketForCellType(IdString cell_type) const
 {
-    if (cell_type.in(id_CC_IBUF, id_CC_OBUF, id_CC_TOBUF, id_CC_IOBUF))
+    if (cell_type.in(id_CC_IBUF, id_CC_OBUF, id_CC_TOBUF, id_CC_IOBUF, id_CC_LVDS_IBUF, id_CC_LVDS_TOBUF,
+                     id_CC_LVDS_OBUF, id_CC_LVDS_IOBUF))
         return id_GPIO;
     else
         return cell_type;
@@ -115,7 +116,8 @@ bool GateMateImpl::isValidBelForCellType(IdString cell_type, BelId bel) const
 {
     IdString bel_type = ctx->getBelType(bel);
     if (bel_type == id_GPIO)
-        return cell_type.in(id_CC_IBUF, id_CC_OBUF, id_CC_TOBUF, id_CC_IOBUF);
+        return cell_type.in(id_CC_IBUF, id_CC_OBUF, id_CC_TOBUF, id_CC_IOBUF, id_CC_LVDS_IBUF, id_CC_LVDS_TOBUF,
+                            id_CC_LVDS_OBUF, id_CC_LVDS_IOBUF);
     else
         return (bel_type == cell_type);
 }
