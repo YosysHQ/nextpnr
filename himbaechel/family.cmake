ab7a3724918b793fbd031c6d222b9786c983af95
Catherine <whitequark@whitequark.org>
2025-01-12 07:13:08 +0000

    himbaechel: allow subsetting uarches. (#1416)
    E.g. selecting only Gowin instead of the default shrinks the resulting
    binary by ~30%.

--- himbaechel/family.cmake
+++ himbaechel/family.cmake
@@ -1,5 +1,15 @@
 set(HIMBAECHEL_UARCHES "example;gowin;xilinx;ng-ultra")
-foreach(uarch ${HIMBAECHEL_UARCHES})
+
+set(HIMBAECHEL_UARCH "${HIMBAECHEL_UARCHES}" CACHE STRING "Microarchitectures for nextpnr-himbaechel build")
+set_property(CACHE HIMBAECHEL_UARCH PROPERTY STRINGS ${HIMBAECHEL_UARCHES})
+
+foreach(item ${HIMBAECHEL_UARCH})
+    if (NOT item IN_LIST HIMBAECHEL_UARCHES)
+        message(FATAL_ERROR "Microarchitecture '${item}' not in list of supported architectures")
+    endif()
+endforeach()
+
+foreach(uarch ${HIMBAECHEL_UARCH})
 	add_subdirectory(${family}/uarch/${uarch})
     aux_source_directory(${family}/uarch/${uarch} HM_UARCH_FILES)
     foreach(target ${family_targets})
