8133dfd4ee97804944dbc83462dbab71ab3a8009
Lofty <dan.ravensloft@gmail.com>
2025-05-12 12:15:20 +0100

    Force clock routing along a specific plane

--- himbaechel/uarch/gatemate/gatemate.cc
+++ himbaechel/uarch/gatemate/gatemate.cc
@@ -21,6 +21,7 @@
 
 #include "gatemate.h"
 #include "design_utils.h"
+#include "log.h"
 #include "placer_heap.h"
 
 #define GEN_INIT_CONSTIDS
@@ -274,6 +275,14 @@ void GateMateImpl::preRoute()
         return port.cell->type.in(id_CPE_HALF,id_CPE_HALF_L,id_CPE_HALF_U) && port.port.in(id_CLK);
     };
 
+    auto pip_plane = [&](PipId pip) {
+        const auto &extra_data =
+                *reinterpret_cast<const GateMatePipExtraDataPOD *>(chip_pip_info(ctx->chip_info, pip).extra_data.get());
+        if (extra_data.type != PipExtra::PIP_EXTRA_MUX)
+            return uint8_t{0};
+        return extra_data.plane;
+    };
+
     for (auto &net : ctx->nets) {
         NetInfo *glb_net = net.second.get();
         if (!glb_net->driver.cell || glb_net->driver.cell->type != id_BUFG)
@@ -292,6 +301,8 @@ void GateMateImpl::preRoute()
         log_info("    routing net '%s'\n", glb_net->name.c_str(ctx));
         ctx->bindWire(ctx->getNetinfoSourceWire(glb_net), glb_net, STRENGTH_LOCKED);
 
+        auto bufg_idx = ctx->getBelLocation(glb_net->driver.cell->bel).z;
+
         for (auto &usr : glb_net->users) {
             std::queue<WireId> visit;
             dict<WireId, PipId> backtrace;
@@ -299,6 +310,8 @@ void GateMateImpl::preRoute()
             // skip arcs that are not part of lowskew routing
             if (!feeds_clk_port(usr))
                 continue;
+
+            auto cpe_loc = ctx->getBelLocation(usr.cell->bel);
 
             auto sink_wire = ctx->getNetinfoSinkWire(glb_net, usr, 0);
             if (ctx->debug) {
@@ -324,6 +337,13 @@ void GateMateImpl::preRoute()
                         continue;
                     if (!ctx->checkWireAvail(src) && ctx->getBoundWireNet(src) != glb_net)
                         continue;
+                    // We need to feed the CPE clock from IM plane 9.
+                    auto pip_loc = ctx->getPipLocation(uh);
+                    if (pip_loc.x == cpe_loc.x && pip_loc.y == cpe_loc.y && (pip_plane(uh) != 9))
+                        continue;
+                    // Otherwise, use only a specific plane.
+                    if ((pip_loc.x != cpe_loc.x || pip_loc.y != cpe_loc.y) && pip_plane(uh) != (9 + bufg_idx))
+                        continue;
                     backtrace[src] = uh;
                     visit.push(src);
                 }
@@ -338,8 +358,8 @@ void GateMateImpl::preRoute()
                     NPNR_ASSERT(glb_net->wires.at(dest).pip == uh);
                     break;
                 }
-                if (ctx->debug)
-                    log_info("            bind pip %s --> %s\n", ctx->nameOfPip(uh), ctx->nameOfWire(dest));
+                //if (ctx->debug)
+                    log_info("            bind pip %s --> %s (plane %hhd)\n", ctx->nameOfPip(uh), ctx->nameOfWire(dest), pip_plane(uh));
                 ctx->bindPip(uh, glb_net, STRENGTH_LOCKED);
                 if (dest == sink_wire)
                     break;
