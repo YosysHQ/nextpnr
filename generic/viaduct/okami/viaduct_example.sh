8063541665c6b79ee0e8ce5ee6ce85b27357661d
Lofty <dan.ravensloft@gmail.com>
2025-09-02 16:55:06 +0100

    okami: remove

--- generic/viaduct/okami/viaduct_example.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-# Run synthesis
-yosys -p "tcl synth_okami.tcl blinky.json" ../../examples/blinky.v
-# Run PnR
-${NEXTPNR:-../../../build/nextpnr-generic} --uarch okami --json blinky.json --router router2
