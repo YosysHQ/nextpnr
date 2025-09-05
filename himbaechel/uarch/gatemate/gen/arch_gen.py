141abe60a69b2bec10cd82958d909d54cc2225c5
Miodrag Milanović <mmicko@gmail.com>
2025-09-05 08:37:29 +0200

    gatemate: cleanup BRAM handling (#1551)

--- himbaechel/uarch/gatemate/gen/arch_gen.py
+++ himbaechel/uarch/gatemate/gen/arch_gen.py
@@ -194,7 +194,7 @@ def set_timings(ch):
         #    assert k in timing, f"pip class {k} not found in timing data"
         #    tmg.set_pip_class(grade=speed, name=k, delay=convert_timing(timing[k]))
 
-EXPECTED_VERSION = 1.6
+EXPECTED_VERSION = 1.7
 
 def main():
     # Range needs to be +1, but we are adding +2 more to coordinates, since 
@@ -234,7 +234,7 @@ def main():
             tt.create_wire(wire.name, wire.type)
         for prim in sorted(die.get_primitives_for_type(type_name)):
             bel = tt.create_bel(prim.name, prim.type, prim.z)
-            if (prim.name in ["CPE_LT_FULL", "CPE_BRIDGE", "RAM"]):
+            if (prim.name in ["CPE_LT_FULL", "CPE_BRIDGE"]):
                 bel.flags |= BEL_FLAG_HIDDEN     
             extra = BelExtraData()
             for constr in sorted(die.get_pins_constraint(type_name, prim.name, prim.type)):
