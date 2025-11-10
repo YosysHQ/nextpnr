69facd7c9aed3d803b06e86ec99d692777af7680
Miodrag Milanovic <mmicko@gmail.com>
2025-11-10 12:05:33 +0100

    Bump gatemate chip database

--- himbaechel/uarch/gatemate/gen/arch_gen.py
+++ himbaechel/uarch/gatemate/gen/arch_gen.py
@@ -224,7 +224,7 @@ def set_timings(ch):
             assert k in timing, f"pip class {k} not found in timing data"
             tmg.set_pip_class(grade=speed, name=k, delay=convert_timing(timing[k]))
 
-EXPECTED_VERSION = 1.9
+EXPECTED_VERSION = 1.10
 
 def main():
     # Range needs to be +1, but we are adding +2 more to coordinates, since 
