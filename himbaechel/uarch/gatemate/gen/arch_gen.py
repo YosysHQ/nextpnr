a9cc738a895a8ac03da3a6e9dd3f03e64ab361a2
Miodrag Milanovic <mmicko@gmail.com>
2026-02-11 10:56:47 +0100

    chipdb version bump

--- himbaechel/uarch/gatemate/gen/arch_gen.py
+++ himbaechel/uarch/gatemate/gen/arch_gen.py
@@ -228,7 +228,7 @@ def set_timings(ch):
             assert k in timing, f"pip class {k} not found in timing data"
             tmg.set_pip_class(grade=speed, name=k, delay=convert_timing(timing[k]))
 
-EXPECTED_VERSION = 1.11
+EXPECTED_VERSION = 1.12
 
 def main():
     # Range needs to be +1, but we are adding +2 more to coordinates, since 
