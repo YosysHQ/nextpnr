55e98cfa19b080c875ece6be0d4e778bb7516d76
Lunaphied <lunaphied@lunaphied.me>
2026-03-27 17:16:28 +0100

    nix: fix issue with latest apycula being needed and it not being in Nixpkgs stable

--- shell.nix
+++ shell.nix
@@ -1,10 +1,25 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/25.11.tar.gz") {} }:
 
 let
-  inherit (pkgs) stdenv lib;
+  inherit (pkgs) stdenv lib fetchPypi;
   inherit (stdenv.hostPlatform) isDarwin;
   pythonPkgs = pkgs.python3Packages;
   boostPython = pkgs.boost.override { python = pythonPkgs.python; enablePython = true; };
+  apycula = let version = "0.31"; in pythonPkgs.apycula.overridePythonAttrs (old: {
+    inherit version;
+
+    src = fetchPypi {
+      inherit version;
+      pname = "apycula";
+      hash = "sha256-77pr4HbS2adFeEI3Q3KzcCfJMi4UomOtKnuGAxobxF0=";
+    };
+
+    dependencies = with pythonPkgs; [
+      msgspec
+      numpy
+      fastcrc
+    ] ++ old.dependencies;
+  });
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     cmake
@@ -12,7 +27,7 @@ in pkgs.mkShell {
     eigen
     boostPython
     pythonPkgs.python
-    pythonPkgs.apycula
+    apycula
     pkgs.qt6.qtbase
     lld
     llvmPackages.openmp
